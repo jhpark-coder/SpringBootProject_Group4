3b784d1ad10f4fbdfacc9615cc9a99159835eecd
Y.Woo <guguwwo@gmail.com>
2025-06-27 15:39:05 +0900

    header css 조정 및 소셜계정으로 일반 로그인시 500error 방지

--- src/main/java/com/creatorworks/nexus/security/SecurityConfig.java
+++ src/main/java/com/creatorworks/nexus/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,7 +25,7 @@ public class SecurityConfig {
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     @SuppressWarnings("removal")
