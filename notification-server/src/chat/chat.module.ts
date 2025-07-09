62b297f99b374e409ea11a8080eaeb0879c2e9e0
jhpark-coder <pjh133765@gmail.com>
2025-07-09 14:51:16 +0900

    aa

--- notification-server/src/chat/chat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatGateway } from './chat.gateway';
-import { ChatService } from './chat.service';
-
-@Module({
-    providers: [ChatGateway, ChatService],
-    exports: [ChatService],
-})
-export class ChatModule { } 
